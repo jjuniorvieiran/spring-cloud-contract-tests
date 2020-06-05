b6cc218bd93a837b19377e557067f3fd7d165b7f
Junior <junior.jaava@gmail.com>
2020-06-05 16:57:00 +0200

    EvenOddController class

---
+++ spring-cloud-contract-producer/src/test/java/com/baeldung/spring/cloud/springcloudcontractproducer/SpringCloudContractProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeldung.spring.cloud.springcloudcontractproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudContractProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
