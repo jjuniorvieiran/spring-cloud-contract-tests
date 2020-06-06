3dac4c67455a513b56248782be6fdda86690a5e8
Junior <junior.jaava@gmail.com>
2020-06-06 14:05:17 +0200

    first consumer propeject  commit

---
+++ spring-cloud-contract-consumer/src/test/java/com/baeldung/spring/cloud/springcloudcontractconsumer/SpringCloudContractConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeldung.spring.cloud.springcloudcontractconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudContractConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
